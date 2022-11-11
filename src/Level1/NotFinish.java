bd5c973bc0c5dc605faa5b66ae862e491ab69e4c
seongjin <nature9545@gmail.com>
2022-11-12 00:12:56 +0900

    프로그래머스 완주하지 못한 선수

---
+++ src/Level1/NotFinish.java
@@ -0,0 +1,17 @@
+package Level1;
+
+import java.util.Arrays;
+
+public class NotFinish {
+    public String solution(String[] participant, String[] completion) {
+        int i = 0;
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        for(i = 0; i < completion.length; i++){
+            if(!participant[i].equals(completion[i])){
+                break;
+            }
+        }
+        return participant[i];
+    }
+}

f19d6d78199e0fb61daa54f13be4f788ba0aee62
seongjin <nature9545@gmail.com>
2022-11-15 20:46:06 +0900

    20221114 프로그래머스 문제풀이

---
+++ src/Level1/FruitSeller.java
@@ -0,0 +1,14 @@
+package Level1;
+
+import java.util.Arrays;
+
+public class FruitSeller {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+        Arrays.sort(score);
+        for(int i = score.length-m; i >= 0; i-=m) {
+            answer += score[i]*m;
+        }
+        return answer;
+    }
+}

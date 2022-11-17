cc311ac3cc05f28f4eb4c151f039f77a307ed111
seongjin <nature9545@gmail.com>
2022-11-17 20:54:44 +0900

    프로그래머스 다트 게임

---
+++ src/Level1/Dart.java
@@ -0,0 +1,48 @@
+package Level1;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class Dart {
+    public int solution(String dartResult) {
+        int answer = 0;
+        String strNum = "";
+        int num = 0;
+        int first = 0;
+        int second = 0;
+        for(int i = 0; i < dartResult.length(); i++){
+            if(dartResult.charAt(i) >= '0' && dartResult.charAt(i) <= '9'){
+                strNum += dartResult.charAt(i);
+            }else{
+                if(dartResult.charAt(i) == 'S'){
+                    num = Integer.parseInt(strNum);
+                    strNum = "";
+                    second = first;
+                    first = num;
+                    answer += first;
+                } else if(dartResult.charAt(i) == 'D'){
+                    num = Integer.parseInt(strNum);
+                    strNum = "";
+                    second = first;
+                    first = num * num;
+                    answer += first;
+                } else if(dartResult.charAt(i) == 'T'){
+                    num = Integer.parseInt(strNum);
+                    strNum = "";
+                    second = first;
+                    first = num * num * num;
+                    answer += first;
+                } else if(dartResult.charAt(i) == '*'){
+                    answer += first + second;
+                    second *= 2;
+                    first *= 2;
+                } else if(dartResult.charAt(i) == '#'){
+                    answer -= first * 2;
+                    first *= -1;
+                }
+
+            }
+        }
+        return answer;
+    }
+}

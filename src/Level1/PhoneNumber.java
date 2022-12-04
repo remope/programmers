ebdda708380282a691eb1877b6f2d06586f62e80
seongjin <nature9545@gmail.com>
2022-12-04 23:08:42 +0900

    프로그래머스 핸드폰 번호 가리기

---
+++ src/Level1/PhoneNumber.java
@@ -0,0 +1,12 @@
+package Level1;
+
+public class PhoneNumber {
+    public String solution(String phone_number) {
+        String answer = "";
+        for(int i = 0; i < phone_number.length() - 4; i++){
+            answer += "*";
+        }
+        answer += phone_number.substring(phone_number.length() - 4, phone_number.length());
+        return answer;
+    }
+}

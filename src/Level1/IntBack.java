655e07a8434274e3ebe075b6921040fd83ce9861
seongjin <nature9545@gmail.com>
2022-12-17 20:22:54 +0900

    프로그래머스 자연수 뒤집어 배열로 만들기

---
+++ src/Level1/IntBack.java
@@ -0,0 +1,15 @@
+package Level1;
+
+import java.util.ArrayList;
+
+public class IntBack {
+    public int[] solution(long n) {
+        String str = Long.toString(n);
+        int length = str.length();
+        int[] answer = new int[length];
+        for(int i = length-1; i >= 0; i--){
+            answer[length - i - 1] = str.charAt(i) - '0';
+        }
+        return answer;
+    }
+}

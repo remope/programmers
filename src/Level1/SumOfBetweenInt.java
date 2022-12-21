44b7cb4f11f4629448aefd71d444134808931450
seongjin <nature9545@gmail.com>
2022-12-21 22:19:51 +0900

    프로그래머스 두 정수 사이의 합

---
+++ src/Level1/SumOfBetweenInt.java
@@ -0,0 +1,13 @@
+package Level1;
+
+public class SumOfBetweenInt {
+    public long solution(int a, int b) {
+        long answer = 0;
+        int start = a > b ? b : a;
+        int end = a > b ? a : b;
+        for(int i = start; i <= end; i++){
+            answer += i;
+        }
+        return answer;
+    }
+}

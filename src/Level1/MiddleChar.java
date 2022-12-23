e182ff9616cb9f12fa067b1bcb24167fa00c6a14
seongjin <nature9545@gmail.com>
2022-12-23 20:40:10 +0900

    프로그래머스 가운데 글자 가져오기

---
+++ src/Level1/MiddleChar.java
@@ -0,0 +1,8 @@
+package Level1;
+
+public class MiddleChar {
+    public String solution(String s) {
+        int length = s.length();
+        return length % 2 == 0 ? s.substring(length / 2 - 1, length / 2 + 1) : s.substring(length / 2, length/2 + 1);
+    }
+}

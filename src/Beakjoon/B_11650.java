2e42e7adfdd94e7ad332fb4f9b4522edeced9239
seongjin <nature9545@gmail.com>
2023-03-02 22:03:28 +0900

    백준 11650

---
+++ src/Beakjoon/B_11650.java
@@ -0,0 +1,39 @@
+package Beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class B_11650 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+
+        int[][] point = new int[n][2];
+
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            point[i][0] = Integer.parseInt(st.nextToken());
+            point[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(point, (a, b) -> {
+            if(a[0] == b[0]) {
+                return a[1] - b[1];
+            } else {
+                return a[0] - b[0];
+            }
+        });
+
+        for(int i = 0; i < n; i++){
+            System.out.println(point[i][0] + " " + point[i][1]);
+        }
+
+    }
+}

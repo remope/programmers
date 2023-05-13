52cf0e5d65ed08cac5fee4a80f2831cdf1c9a303
seongjin <nature9545@gmail.com>
2023-05-14 00:04:04 +0900

    백준 1026

---
+++ src/Beakjoon/B_1026.java
@@ -0,0 +1,41 @@
+package Beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class B_1026 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        Integer[] a = new Integer[n];
+        Integer[] b = new Integer[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < n; i++) {
+            b[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(a);
+        Arrays.sort(b, Collections.reverseOrder());
+
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            result += a[i] * b[i];
+        }
+        System.out.println(result);
+
+    }
+}

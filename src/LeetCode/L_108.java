dc01ad449e058a3803b72c67defcbc07acfcfc78
seongjin <nature9545@gmail.com>
2024-01-25 16:16:54 +0900

    LeetCode 108

--- src/LeetCode/L_108.java
+++ src/LeetCode/L_108.java
@@ -1,2 +1,21 @@
-package LeetCode;public class L_108 {
+package LeetCode;
+
+import LeetCode.DataStructure.TreeNode;
+
+public class L_108 {
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return makeBST(nums, 0, nums.length - 1);
+    }
+    public TreeNode makeBST(int[] nums, int start, int end) {
+        if(start > end) {
+            return null;
+        }
+
+        int mid = (start + end) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = makeBST(nums, start, mid - 1);
+        root.right = makeBST(nums, mid + 1, end);
+        return root;
+    }
 }
